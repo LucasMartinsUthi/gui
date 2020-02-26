c8daecad4d58627c4c72b00025bb9e099d60f27f
LucasMartinsUthi <luscagbr@gmail.com>
2020-02-26 10:47:31 -0300

    gui: react init

---
+++ gui/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/style.scss';
+
+export default class App extends React.Component {
+    render() {
+        return (
+            <div>
+            <h1>GUI</h1>
+            <p>hello world!</p>
+            </div>
+        )
+    
+    }
+}
