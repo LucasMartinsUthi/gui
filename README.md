dcecf2ebd8691d4fb9a2c189739d73145a1b3722
LucasMartinsUthi <luscagbr@gmail.com>
2020-02-27 03:01:15 -0300

    gui: Navbar

--- README.md
+++
@@ -1,5 +0,0 @@
-# Furgbol GUI
-This project is a web-based GUI for controlling Robocup's VSSS Furgbol software.
-
-## Contents
-Upcoming.
--- gui/src/App.js
+++ gui/src/App.js
@@ -1,15 +1,22 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
 import './styles/style.scss';
+import Navbar from './components/Navbar';
 
 export default class App extends React.Component {
     render() {
         return (
-            <div>
-            <h1>GUI</h1>
-            <p>hello world!</p>
+            <div style={{display: 'flex', flexDirection: 'column', height: '100%'}}>
+                <Navbar />
+                <div className="container">
+                    {/* TODO Transformar content em um component 
+                        recebe como propriedade qual pagina carregar
+                        assim posso separar as paginas em arquivos diferentes    
+                    */}
+                    <div className="content">
+                        {/* vazio */}
+                    </div>
+                </div>
             </div>
         )
-    
     }
 }
